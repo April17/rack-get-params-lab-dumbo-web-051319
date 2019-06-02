5124cd9b54030ab3f8ef6877b114ba87a0b36b75
Fanqiang Meng <fanmeng0417@gmail.com>
2019-06-02 18:35:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,20 +11,33 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+
     elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
+
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+
+    elsif req.path.match(/add/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+
     else
       resp.write "Path Not Found"
     end
 
     resp.finish
   end
-
+  def handle_add(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
